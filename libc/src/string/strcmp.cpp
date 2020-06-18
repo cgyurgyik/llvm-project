244ded0a10832f473129f8f264293a80431cc5bb
cgyurgyik <gyurgyikcp@gmail.com>
2020-06-18 19:47:45 -0400

    Add strcmp.

---
+++ libc/src/string/strcmp.cpp
@@ -0,0 +1,24 @@
+//===-- Implementation of strcmp ------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/string/strcmp.h"
+
+#include "src/__support/common.h"
+
+namespace __llvm_libc {
+
+int LLVM_LIBC_ENTRYPOINT(strcmp)(const char *l, const char *r) {
+  while (*l) {
+    if (*l != *r) break;
+    ++l;
+    ++r;
+  }
+  return *(const unsigned char*)l - *(const unsigned char*)r;
+}
+
+} // namespace __llvm_libc
